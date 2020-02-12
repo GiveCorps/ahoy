6efc40f6be7d0d9689c2935a6ea1e8d924d0be80
Harsh <pathakh@msn.com>
2020-02-11 20:26:22 -0500

    change skip_filter to skip_before_action, skip_after_action, and skip_around_action

--- app/controllers/ahoy/base_controller.rb
+++ app/controllers/ahoy/base_controller.rb
@@ -2,11 +2,9 @@ module Ahoy
   class BaseController < ApplicationController
     # skip all filters except for authlogic
     filters = _process_action_callbacks.map(&:filter) - [:load_authlogic]
-    if respond_to?(:skip_action)
-      skip_action *filters
-    else
-      skip_filter *filters
-    end
+    skip_before_action(*filters, raise: false)
+    skip_after_action(*filters, raise: false)
+    skip_around_action(*filters, raise: false)
 
     def ahoy
       @ahoy ||= Ahoy::Tracker.new(controller: self, api: true)
